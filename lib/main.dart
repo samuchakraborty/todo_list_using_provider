200ca38d27845395045edd0a16fcc89364d0d78a
samuchakraborty <samuckb@gmail.com>
2021-05-10 22:52:33 +0600

    todolist project init

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:todo_list/screens/task_screens.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+
+  Widget build(BuildContext context) {
+    return MaterialApp(
+     
+      home: TasksScreen(),
+    );
+  }
+}
