9e4efb4bc34de8616380ba7e1dad2d7b254dc075
Samu Chakraborty <61682653+samuchakraborty@users.noreply.github.com>
2021-05-13 00:49:09 +0600

    provider package implemented

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:todo_list/screens/task_screens.dart';
+
+import 'model/tasks_data.dart';
 
 void main() {
   runApp(MyApp());
@@ -7,9 +10,12 @@ void main() {
 
 class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: TasksScreen(),
+    return ChangeNotifierProvider<TaskData>(
+      create: (_) => TaskData(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: TasksScreen(),
+      ),
     );
   }
 }
